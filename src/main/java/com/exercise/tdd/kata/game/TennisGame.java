93a2c69b001cbf11f4f4a380434bb2ef22f6ec0b
Thiago Ferreira <tronikansas@hotmail.com>
2021-08-21 14:34:52 +0100

    refactoring the class Tennis Game,

--- src/main/java/com/exercise/tdd/kata/game/TennisGame.java
+++ src/main/java/com/exercise/tdd/kata/game/TennisGame.java
@@ -1,6 +1,8 @@
 package com.exercise.tdd.kata.game;
 
 import com.exercise.tdd.kata.game.player.Player;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -9,22 +11,18 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class TennisGame {
 
+  private List<String> setPoints = new ArrayList(Arrays.asList("love", "fifteen", "thirty", "forty"));
+
   public String viewScore(List<Player> playerList){
     if(playerList == null || playerList.size() != 2){
       return "To start the Game we need 2 players"
 ;   }else if(playerList.get(0) == null || playerList.get(1) == null){
       return "Please, send a valid player";
     }
-    if(playerList.get(0).getScore() == playerList.get(1).getScore()){
-      if(playerList.get(0).getScore() == 0){
-        return "love all";
-      }else if(playerList.get(0).getScore() == 1){
-        return "fifteen all";
-      }else if(playerList.get(0).getScore() == 2){
-        return "thirty all";
-      }else{
-        return "deuce";
-      }
+    Player player1 =  playerList.get(0);
+    Player player2 = playerList.get(1);
+    if(player1.getScore() == player2.getScore()){
+      return player1.getScore() > 2? "deuce": setPoints.get(player1.getScore()) + " all";
     }
     return "score  fail";
   }

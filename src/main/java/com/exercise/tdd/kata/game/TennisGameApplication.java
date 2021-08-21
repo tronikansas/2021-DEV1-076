e7fa081ffd5fb6961046370d6b66adc3b03f5bc8
tronikansas <89300138+tronikansas@users.noreply.github.com>
2021-08-21 12:18:24 +0100

    first commit

---
+++ src/main/java/com/exercise/tdd/kata/game/TennisGameApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.tdd.kata.game;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TennisGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TennisGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exercise/tdd/kata/game/TennisGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.tdd.kata.game;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TennisGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

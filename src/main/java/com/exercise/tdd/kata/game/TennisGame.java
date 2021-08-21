5db58b0915889383810598daf86c376a71fafef1
Thiago Ferreira <tronikansas@hotmail.com>
2021-08-21 15:40:25 +0100

    making improvements in the tennisGame class

--- src/main/java/com/exercise/tdd/kata/game/TennisGame.java
+++ src/main/java/com/exercise/tdd/kata/game/TennisGame.java
@@ -4,10 +4,9 @@ import com.exercise.tdd.kata.game.player.Player;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Controller;
 
-@NoArgsConstructor
+
 @Controller
 public class TennisGame {
 
@@ -15,21 +14,18 @@ public class TennisGame {
 
   public String viewScore(List<Player> playerList) {
     if (playerList == null || playerList.size() != 2) {
-      return "To start the Game we need 2 players"
-          ;
+      return "To start the Game we need 2 players";
     } else if (playerList.get(0) == null || playerList.get(1) == null) {
       return "Please, send a valid player";
     }
     Player player1 = playerList.get(0);
     Player player2 = playerList.get(1);
-    if (player1.getScore() == player2.getScore()) {
-      return player1.getScore() > 2 ? "deuce" : setPoints.get(player1.getScore()) + " all";
-    } else {
-      if (Math.max(player1.getScore(), player2.getScore()) > 3) {
-
+    if(player1.getScore() == player2.getScore()){
+      return player1.getScore() > 2 ? "deuce": setPoints.get(player1.getScore()) + " all";
+    }else{
+      if(Math.max(player1.getScore(), player2.getScore()) > 3){
         return diffResult(player1, player2);
-
-      } else {
+      }else{
         return setPoints.get(player1.getScore()) + " - " + setPoints.get(player2.getScore());
       }
     }
@@ -37,13 +33,14 @@ public class TennisGame {
 
   private String diffResult(Player player1, Player player2) {
     int diff = Math.abs(player1.getScore() - player2.getScore());
-
-    if (diff > 2) {
+    if(diff > 2){
       return "Impossible score according to the rules";
-    } else if (diff == 1){
-      return "Advantage " + ((player1.getScore() > player2.getScore())? player1.getName(): player2.getName());
-    } else{
-      return  "Game over the winner is " + ((player1.getScore() > player2.getScore())? player1.getName(): player2.getName());
+    }else {
+      return  (diff == 1 ? "Advantage " : "Game over the winner is ") + getPlayerWithHigherScore(player1, player2);
     }
+  }
+
+  private String getPlayerWithHigherScore(Player player1, Player player2) {
+    return player1.getScore() > player2.getScore()? player1.getName(): player2.getName();
   }
 }
